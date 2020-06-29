336444f121472429a698ab2a43d5c6b0af402969
kashyap <sreesha.kashyap@mavs.uta.edu>
2020-06-29 18:24:59 -0500

    Updating project files

---
+++ PetClinicApplication/src/test/java/edu/learnlab/uta/petclinicapplication/PetclinicapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.learnlab.uta.petclinicapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
