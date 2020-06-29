3d96e735f269853e7bf3f193734c7418714f08e1
kashyap <sreesha.kashyap@mavs.uta.edu>
2020-06-29 18:07:07 -0500

    Adding project files

---
+++ PetClinicApplication/src/main/java/edu/learnlab/uta/petclinicapplication/PetclinicapplicationApplication.java
@@ -0,0 +1,13 @@
+package edu.learnlab.uta.petclinicapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicapplicationApplication.class, args);
+    }
+
+}
